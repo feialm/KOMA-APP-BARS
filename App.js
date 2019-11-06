0c6c7ef88e690f21834d31342d0ead969433e234
feialm <45712644+feialm@users.noreply.github.com>
2019-11-06 13:20:39 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// <img src={logo} className="App-logo" alt="logo" />
+
+
+function App() {
+  return (
+    <div classname ="App">
+      <div className="App-header">
+        <h1> APPNAME </h1>
+      </div>
+
+
+    <div classname ="MainBody">
+      <p> hej </p>
+    </div>
+
+   <div classname="App-bottom">
+      <p> hejdå </p>
+      </div>
+
+  </div>
+
+  );
+}
+
+export default App;

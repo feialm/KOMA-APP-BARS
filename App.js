d25525b27334053ed00822786a5887789b58b3bb
jesschowh <57443463+jesschowh@users.noreply.github.com>
2019-11-06 16:41:48 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container, Button, Link, lightColors, darkColors} from 'react-floating-action-button'
 
-// <img src={logo} className="App-logo" alt="logo" />
+
+function ActivityInfo(){
+return(
+  <div className="WrapperActivity">
+          <p className="ActivityName">activity</p>
+          <div className="ActivityDetails">
+            <p className="ActivityPercentage">percent</p>
+            <p className="ActivityMoreDetails">More details</p>
+            </div>
+
+          <div className="ActivityBar">
+          </div>
+          <div className="ActivityProgress">
+          </div>
+        </div>
+        )
+}
+
 
 
 function App() {
   return (
-    <div classname ="App">
+    <div className="App">
       <div className="App-header">
-        <h1> APPNAME </h1>
+        <h1>APPNAME</h1>
+      </div>
+{/*<Activity name='tnm040' progress=´95%´ />*/}
+{/*new array activities = ['Swe', 'Den', 'Fin'];*/}
+
+{/*activities.*/}
+
+ <Container className="QuestionButton">
+    <Button 
+     styles={{backgroundColor: darkColors.lighterRed, color: lightColors.black, height: 40, width: 40}}
+     />
+</Container>
+      <div className="MainBody">
+        {/*här ska vi köra ActivityInfo*/}
+      </div>
+
+  <Container className="MenuButton">
+    <Button 
+     styles={{backgroundColor: darkColors.lighterRed, color: lightColors.white}}
+     />
+</Container>
+
+      <div className="App-bottom">
+      <p className="Dots">Bottom Header</p>
       </div>
 
 
-    <div classname ="MainBody">
-      <p> hej </p>
     </div>
-
-   <div classname="App-bottom">
-      <p> hejdå </p>
-      </div>
-
-  </div>
-
   );
 }
 

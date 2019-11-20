5dbd0d497e001d6d40e46395fe0b4f5e796600c3
jesschowh <57443463+jesschowh@users.noreply.github.com>
2019-11-20 17:16:30 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 
 // fake array to test stuff
@@ -15,33 +15,33 @@ var activityArray = [a1, a2, a3, a4, a5, a6];
 
 let i = 0;
 
-function colourID(i){
-
-i++;
-return i;
+function incrementID(){
+  i++;
 }
 
 function colour(i){
  
   let colour1 = "white";
 
-  var mod = colourID(i) % 5;
+  var mod = i % 5;
 
   if (mod === 0){
-    colour1 = "red";
+    colour1 = "#f92672"; //rosa
   }
-   if (mod === 1){
-    colour1 = "blue";
+   else if (mod === 1){
+    colour1 = "#a6e22e"; //grön #33c474
   }
    if (mod === 2){
-     colour1 = "green";
+     colour1 = "#66d9ef" //blå #4ac3ff
   }
    if (mod === 3){
-     colour1 = "yellow";
+     colour1 = "#ae81ff"; //lila #ca45ff
   }
    if (mod === 4){
-    colour1 = "brown";
+    colour1 = "#fd971f"; //orange
   }
+
+  incrementID();
   return colour1;
 }
 
@@ -66,16 +66,15 @@ function handleClickQuestion(e) {
   console.log('hej du klickade rätt typ igen');
 }
 
-function handleClickMenu(e) {
-  e.preventDefault();
-  console.log('GRATTIS DU KAN KLICKA typ');
-}
+
+
 
 /*Funktion som ger innehåll genom att skicka in array av objekt*/
 function ActivityInfo(props) {
 
   const a = props.a;
   let barProgress = timeCalc(a.totTime, a.repTime)+"%";
+  var same = colour(i);
 
   return(
     <div className="WrapperActivity" onClick={handleClickActivity}>
@@ -86,20 +85,61 @@ function ActivityInfo(props) {
           <p className="ActivityPercentage">{timeCalc(a.totTime, a.repTime)}% done</p>
           <p className="ActivityMoreDetails">More details</p>
       </div>
-
-      <div className="ActivityBar" style={{backgroundColor: colour(i)}}></div>
-      <div className="ActivityProgress" style={{width: barProgress}}></div>
+      
+      <div className="ActivityBar" style={{backgroundColor: same, opacity: 0.3}}></div>
+      <div className="ActivityProgress" style={{width: barProgress, backgroundColor: same, opacity: 0.8}}></div>
     
     </div>
   )
 }
 
 
+  function reportTime(){
+  return(
+    <p>Report Time</p>
+    )
+}
+
 /*renderar sidelementen typ*/
-function App() {
+function App(props) {
+i = 0;
+const [menuWidth, changeMenuWidth] = useState(0);
+const [buttonWidth, changeButtonWidth] = useState(0);
+
+  function HandleClickMenu(){
+      
+    changeMenuWidth("100%");
+    changeButtonWidth("250px");
+   
+    console.log('GRATTIS DU KAN KLICKA typ');
+  }
+
+ function reportTime(){
+  if (buttonWidth === "250px"){
+    return(
+      <p>Report Time</p>
+      );
+  }
+ }
+
+ function addActivity(){
+  if (buttonWidth === "250px"){
+    return(
+      <p>Add Activity</p>
+      );
+  }
+ }
+
+  function deleteActivity(){
+  if (buttonWidth === "250px"){
+    return(
+      <p>Delete Activity</p>
+      );
+  }
+ }
   return (
     <div className="App">
-
+ 
       {/*Header*/}
       <div className="App-header">
         <h1>APPNAME</h1>
@@ -113,6 +153,7 @@ function App() {
       {/*Main body*/}
       <div className="MainBody">
         {renderActivities()}  {/*här ska vi köra ActivityInfo, för att skriva ut kursinfo*/}
+    
       </div>
 
       {/*Footer*/}
@@ -120,8 +161,16 @@ function App() {
       <p className="Dots">Bottom Header</p>
       </div>
 
+       <div id="menuID" style={{width: menuWidth}}></div>
+
+        <div className="buttonWrapper" style={{width: buttonWidth}}>
+        <div className="reportButton" style={{width: buttonWidth}}>{reportTime()}</div>
+        <div className="addButton" style={{width: buttonWidth}}>{addActivity()}</div>
+        <div className="deleteButton" style={{width: buttonWidth}}>{deleteActivity()}</div>
+       </div>
+
       {/*Menu Button*/}
-      <div className="MenuButton" onClick={handleClickMenu}>
+      <div className="MenuButton" onClick={HandleClickMenu}>
         <div className="MenuIcon">
           <div className="menu"></div>
           <div className="menu"></div>

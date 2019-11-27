d8cae496b08a8e9e63cdc22c2f7537f20334e37a
jesschowh <57443463+jesschowh@users.noreply.github.com>
2019-11-27 15:17:36 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { useState } from 'react';
 
 
 // fake array to test stuff
-var a1 = {name:"TNA003", totTime:"100", repTime:"70"};
-var a2 = {name:"TNG033", totTime:"100", repTime:"30"};
+var a1 = {name:"TNA001", totTime:"100", repTime:"70"};
+var a2 = {name:"TNG032", totTime:"100", repTime:"30"};
 var a3 = {name:"TNG033", totTime:"100", repTime:"30"};
-var a4 = {name:"TNA003", totTime:"100", repTime:"70"};
-var a5 = {name:"TNG033", totTime:"100", repTime:"66"};
-var a6 = {name:"TNG033", totTime:"100", repTime:"30"};
+var a4 = {name:"TNA004", totTime:"100", repTime:"70"};
+var a5 = {name:"TNG035", totTime:"100", repTime:"66"};
+var a6 = {name:"TNG036", totTime:"100", repTime:"30"};
+var a7 = {name:"HEJjosefin", totTime:"100", repTime:"100"};
+var a8 = {name:"AWESOMENESS of Sofie", totTime:"100", repTime:"100"};
 
-var activityArray = [a1, a2, a3, a4, a5, a6];
+var activityArray = [a1, a2, a3, a4, a5, a6, a7, a8]; //aktiviteter utan färger
 
-let i = 0;
+var activityColor = []; //aktiviteter med färger 
 
-function incrementID(){
+let i = 0; //färg
+
+function incrementID(){  //färg
   i++;
 }
 
-function colour(i){
+function colour(i){  //färg 
  
   let colour1 = "white";
 
@@ -45,7 +49,7 @@ function colour(i){
   return colour1;
 }
 
-function timeCalc(total, part){
+function timeCalc(total, part){       //beräknar barProgress samt procent
   var percentage = (part/total)*100;
   return percentage;
 }
@@ -58,15 +62,8 @@ function renderActivities(){
 
 function handleClickActivity(e) {
   e.preventDefault();
-  console.log('hej du klickade rätt typ');
+  console.log('hej du klickade på kursen');
 }
-
-function handleClickQuestion(e) {
-  e.preventDefault();
-  console.log('hej du klickade rätt typ igen');
-}
-
-
 
 
 /*Funktion som ger innehåll genom att skicka in array av objekt*/
@@ -76,12 +73,19 @@ function ActivityInfo(props) {
   let barProgress = timeCalc(a.totTime, a.repTime)+"%";
   var same = colour(i);
 
+  var oneActivity = [a];
+
+  oneActivity.push(same);
+  activityColor.push(oneActivity);
+
+  console.log(activityColor);
+
   return(
-    <div className="WrapperActivity" onClick={handleClickActivity}>
+    <div className="WrapperActivity" onClick={handleClickActivity}>  
 
       {/*Här ska vi försöka skriva ut arrayen med dess namn och tid */}
       <p className="ActivityName">{a.name}</p>
-      <div className="ActivityDetails">
+      <div className="ActivityDetails">      {/* info i aktivitets-rutan */}
           <p className="ActivityPercentage">{timeCalc(a.totTime, a.repTime)}% done</p>
           <p className="ActivityMoreDetails">More details</p>
       </div>
@@ -94,88 +98,123 @@ function ActivityInfo(props) {
 }
 
 
-  function reportTime(){
-  return(
-    <p>Report Time</p>
-    )
-}
-
 /*renderar sidelementen typ*/
 function App(props) {
   i = 0;
-  const [menuWidth, changeMenuWidth] = useState(0);
-  const [buttonWidth, changeButtonWidth] = useState(0);
-  const [hideMenu, triggerHideMenu] = useState(true); 
 
-  function HandleClickMenu(){ {/*gör likadant för allt annat, typ?*/}
-    if(hideMenu){
-      triggerHideMenu(false);
-      changeMenuWidth("100%");
-      changeButtonWidth("250px");
+  /*meny lista typ*/
+  const [menuWidth, changeMenuWidth] = useState(0);     
+  const [buttonWidth, changeButtonWidth] = useState(0);
+  const [hideMenu, triggerHideMenu] = useState(true);
+  const [hideQuestion, triggerHideQuestion] = useState(true); 
+  const [questionWidth, changeQuestionWidth] = useState(0); 
+  const [questionOpacity, changeQuestionOpacity] = useState("100%"); 
+  const [closeQuestionOpacity, changeCloseQuestionOpacity] = useState("0%");
+
+  /* om man klickar på question button */
+  function handleClickQuestion() {
+    if(hideQuestion){
+      triggerHideQuestion(false);
+      changeQuestionWidth("100%");
+      changeQuestionOpacity("0%");
+      changeCloseQuestionOpacity("100%");
     }
     else{
-      triggerHideMenu(true);
+      triggerHideQuestion(true);
+      changeQuestionWidth("0");
+      changeQuestionOpacity("100%");
+      changeCloseQuestionOpacity("0%");
+    }
+    console.log('hej du klickade på frågetecknet');
+  }
+
+  function questionText(){
+    if(hideQuestion === false){
+      return(
+        <div className="questionWrapper">
+          <div className="App-header">
+          <h1>HELP<br/></h1>
+          </div>
+          <div className="questionText">
+            <p className="helpTitle">To do List</p>
+            <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+         <p className="helpTitle">To do List</p>
+            <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+         <p className="helpTitle">To do List</p>
+            <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+         <p className="helpTitle">To do List</p>
+            <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+         <p className="helpTitle">To do List</p>
+            <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+         
+          </div>
+        </div>
+      );
+    }
+  }
+
+  /* om man klickar på meny-knappen */
+  function HandleClickMenu(){       /*gör likadant för allt annat, typ?*/
+    if(hideMenu){                   /* öppnar menyn */
+      triggerHideMenu(false);
+      changeMenuWidth("100%");
+      changeButtonWidth("200px");
+    }
+    else{                           /* göm menyn */
+      triggerHideMenu(true);  
       changeMenuWidth("0");
       changeButtonWidth("0");
     }
-    console.log('GRATTIS DU KAN KLICKA typ');
   }
 
   function reportTime(){
-  if (buttonWidth === "250px"){
+  if (hideMenu === false){
     return(
-      <p>Report Time</p>
+      <p className="buttonText">Report Time</p>
       );
   }
   }
 
   function addActivity(){
-  if (buttonWidth === "250px"){
+  if (hideMenu === false){
     return(
-      <p>Add Activity</p>
+      <p className="buttonText">Add Activity</p>
       );
   }
   }
 
   function deleteActivity(){
-    if (buttonWidth === "250px"){
+    if (hideMenu === false){
       return(
-        <p>Delete Activity</p>
+        <p className="buttonText">Delete Activity</p>
         );
     }
   }
+
   return (
     <div className="App">
  
       {/*Header*/}
       <div className="App-header">
         <h1>APPNAME</h1>
+      </div>
 
-        {/*Question Button*/}
-        <div className="QuestionButton" onClick={handleClickQuestion}>
-         <p className="question">?</p>
-        </div>
+      {/*Question Button*/}
+      <div className="QuestionButton" onClick={handleClickQuestion}>
+         <p className="question" style={{opacity: questionOpacity}}>?</p>
+         <p className="closeQuestion" style={{opacity: closeQuestionOpacity}}>x</p>
       </div>
 
       {/*Main body*/}
       <div className="MainBody">
-        {renderActivities()}  {/*här ska vi köra ActivityInfo, för att skriva ut kursinfo*/}
-    
+        {renderActivities()}  {/*här körs ActivityInfo för att skriva ut kursinfo*/}
       </div>
 
       {/*Footer*/}
       <div className="App-bottom">
       <p className="Dots">Bottom Header</p>
       </div>
-
     
-       <div className="menuID" style={{width: menuWidth}}></div>
-       {/*<div className="buttonWrapper" style={{width: buttonWidth}}>*/}
-        <div className="reportButton" style={{width: buttonWidth}}>{reportTime()}</div>
-        <div className="addButton" style={{width: buttonWidth}}>{addActivity()}</div>
-        <div className="deleteButton" style={{width: buttonWidth}}>{deleteActivity()}</div>
-       {/*</div>*/}
-
       {/*Menu Button*/}
       <div className="MenuButton" onClick={HandleClickMenu}>
         <div className="MenuIcon">
@@ -184,6 +223,19 @@ function App(props) {
           <div className="menu"></div>
         </div>
       </div>
+
+      {/*Menu List*/}
+      <div className="menuClass" style={{width: menuWidth}}>
+      <div className="menuID" style={{width: menuWidth}}></div>
+      <div className="reportButton" style={{width: buttonWidth}}>{reportTime()}</div>
+      <div className="addButton" style={{width: buttonWidth}}>{addActivity()}</div>
+      <div className="deleteButton" style={{width: buttonWidth}}>{deleteActivity()}</div>
+      </div>
+      {/*Question List*/}
+      <div className="questionID" style={{width: questionWidth}}>
+        {questionText()}
+      </div>
+
     </div>
   );
 }

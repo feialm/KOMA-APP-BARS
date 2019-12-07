177a91b1cc1e3e2676400324386777c787dd6de2
xang23 <46744878+xang23@users.noreply.github.com>
2019-12-07 17:37:19 +0100

    Wilma betjänt
    App.css är uppdaterad så att alla classNames börjar med lowercase bokstäver. Har även förkortat.
    Detta har lett till vissa ändringar i className i övriga sidors .js

---
+++ BarSite.js
@@ -0,0 +1,313 @@
+
+//import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './App.css';
+import { useState } from 'react';
+import React, { Component } from 'react';
+import ReportTime from "./ReportTime.js";
+import CourseInfo from './CourseInfo.js';
+
+
+
+
+
+let i = 0;
+
+function incrementID(){
+  i++;
+}
+
+function colour(i){
+ 
+  let colour1 = "white";
+
+  var mod = i % 5;
+
+  if (mod === 0){
+    colour1 = "#f92672"; //rosa
+  }
+   else if (mod === 1){
+    colour1 = "#a6e22e"; //grön #33c474
+  }
+   if (mod === 2){
+     colour1 = "#66d9ef" //blå #4ac3ff
+  }
+   if (mod === 3){
+     colour1 = "#ae81ff"; //lila #ca45ff
+  }
+   if (mod === 4){
+    colour1 = "#fd971f"; //orange
+  }
+
+  incrementID();
+  return colour1;
+}
+
+function timeCalc(total, part){
+  var percentage = (part/total)*100;
+  percentage = percentage.toFixed(2);
+  console.log(total);
+  console.log(part);
+  return percentage;
+}
+
+function gotoCourseInfo(props){
+  console.log(props);
+  
+  console.log("hahahej")
+    return(
+    <CourseInfo test={props}/>
+ );
+}
+
+function handleClickActivity(props) {
+  console.log("grattis du klicka på aktivteten")
+  gotoCourseInfo(props);
+}
+
+function handleClickQuestion(e) {
+  e.preventDefault();
+  console.log(' du klickade rätt typ igen');
+}
+
+
+
+ //denna eller funktion gör vi i falla att det är null i localstarage
+  function loadData(){
+    //const q = loadData();
+
+    try {
+      const storage = JSON.parse(window.localStorage.getItem("data"));
+
+      return storage || [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+
+function renderActivities(HHH){
+  //const b = HHH.courses;
+  const b = HHH;
+ //  console.log(b[0].name);
+
+ //console.log(HHH.name);
+
+
+ return (
+     HHH.map(c=> (<ActivityInfo key={c.name} a={c} />))
+
+    );
+}
+
+
+/*Funktion som ger innehåll genom att skicka in array av objekt*/
+function ActivityInfo(HHH) {
+  const a = HHH.a;
+console.log("eheheh");
+console.log(a);
+  let barProgress = timeCalc(a.totTime, a.repTime)+"%";
+  var same = colour(i);
+  
+  return(
+    <Link to="/CourseInfo.js">
+    <div className="wrapperActivity"  onClick={() => handleClickActivity(a)}>
+
+      {/*Här ska vi försöka skriva ut arrayen med dess namn och tid */}
+      <p className="activityName">{a.id}</p>
+      <div className="activityDetails">
+          <p className="activityPercentage">{timeCalc(a.totTime, a.repTime)}% done</p>
+          <p className="activityMoreDetails">More details</p>
+      </div>
+      
+      <div className="activityBar" style={{backgroundColor: same, opacity: 0.3}}></div>
+      <div className="activityProgress" style={{width: barProgress, backgroundColor: same, opacity: 0.8}}></div>
+    
+    </div>
+    </Link>
+  )
+}
+
+
+//}//den här är tillaggd var sist förut!!!!!!!!!!!!
+/*renderar sidelementen typ*/
+  function BarSite(HHH) {
+    // test
+    const [ToCourseInfo, setToCourseInfo] = useState(0);
+    
+
+    i = 0;
+    var HHH = loadData();
+
+   console.log("JOOOJJJ");
+   console.log(loadData());
+  console.log("Elin");
+ 
+  console.log("Pankaka");
+  console.log(HHH);
+  console.log("majs");
+
+     console.log("Pappa");
+       console.log(HHH.length);
+       console.log("Mamma");
+
+    /*meny lista typ*/
+    const [menuWidth, changeMenuWidth] = useState(0);     
+    const [buttonWidth, changeButtonWidth] = useState(0);
+    const [hideMenu, triggerHideMenu] = useState(true);
+    const [hideQuestion, triggerHideQuestion] = useState(true); 
+    const [questionWidth, changeQuestionWidth] = useState(0); 
+    const [questionOpacity, changeQuestionOpacity] = useState("100%"); 
+    const [closeQuestionOpacity, changeCloseQuestionOpacity] = useState("0%");
+   // const [redirect, setRedirect] = useState(false);
+
+
+
+
+    /* om man klickar på question button */
+    function handleClickQuestion() {
+      if(hideQuestion){
+        triggerHideQuestion(false);
+        changeQuestionWidth("100%");
+        changeQuestionOpacity("0%");
+        changeCloseQuestionOpacity("100%");
+       
+      }
+      else{
+        triggerHideQuestion(true);
+        changeQuestionWidth("0");
+        changeQuestionOpacity("100%");
+        changeCloseQuestionOpacity("0%");
+      }
+      console.log(' du klickade på frågetecknet');
+    }
+
+    function questionText(){
+      if(hideQuestion === false){
+        return(
+          <div className="questionWrapper">
+            <div className="appHeader">
+            <h1 className="title">HELP<br/></h1>
+            </div>
+            <div className="questionText">
+              <p className="helpTitle">To do List</p>
+              <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+              <p className="helpTitle">To do List</p>
+              <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+              <p className="helpTitle">To do List</p>
+              <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+              <p className="helpTitle">To do List</p>
+              <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+              <p className="helpTitle">To do List</p>
+              <p className="helpText">-centera meny √ <br/> -skriva text till help <br/> -hitta på namn <br/> -footer swipe</p>
+            </div>
+          </div>
+        );
+      }
+    }
+
+
+     /* om man klickar på meny-knappen */
+    function HandleClickMenu(){       /*gör likadant för allt annat, typ?*/
+      if(hideMenu){                   /* öppnar menyn */
+        triggerHideMenu(false);
+        changeMenuWidth("100%");
+        changeButtonWidth("200px");
+      }
+      else{                           /* göm menyn */
+        triggerHideMenu(true);  
+        changeMenuWidth("0");
+        changeButtonWidth("0");
+      }
+    }
+
+  function reportTime(){
+    if (hideMenu === false){
+      return(
+        <p className="buttonText">Report Time</p>
+      );
+    }
+  }
+/*
+  function handleClickReport() {
+  console.log(' du klickade på Report');
+   setRedirect(true);
+}
+*/
+  function addActivity(){
+    if (hideMenu === false){
+      return(
+        <p className="buttonText">Add Activity</p>
+      );
+    }
+  }
+
+  function deleteActivity(){
+    if (hideMenu === false){
+      return(
+        <p className="buttonText">Delete Activity</p>
+      );
+    }
+  }
+
+  return (
+    <div className="center">
+ 
+      {/*Header*/}
+      <div className="appHeader head">
+        <h1 className="title">APPNAME</h1>
+         <p className="h3">My Activities</p>
+      </div>
+
+      {/*Question Button*/}
+      <div className="questionButton" onClick={handleClickQuestion}>
+       <p className="question removeMargin">?</p>
+      </div>
+
+      {/*Main body*/}
+      <div className="mainBody">
+        {renderActivities(HHH)}  {/*här ska vi rendera ActivityInfo, för att skriva ut kursinfo*/}
+      </div>
+
+      {/*Footer*/}
+      <div className="App-bottom">
+      <p className="dots">Bottom Header</p>
+      </div>
+
+      {/*Menu Button*/}
+      <div className="menuButton" onClick={HandleClickMenu}>
+        <div className="menuIcon">
+          <div className="menu"></div>
+          <div className="menu"></div>
+          <div className="menu"></div>
+        </div>
+      </div>
+
+      {/*Menu List*/}
+   
+      <div className="menuClass" style={{width: menuWidth}}>
+        <div className="menuID" style={{width: menuWidth}}></div>
+        <Link to="/ReportTime.js"><button className="reportButton" style={{width: buttonWidth}}>{reportTime()}</button></Link>
+        <button className="addButton" style={{width: buttonWidth}}>{addActivity()}</button>
+        <button className="deleteButton" style={{width: buttonWidth}}>{deleteActivity()}</button>
+      </div>
+      
+      {/*Question List*/}
+      <div className="questionID" style={{width: questionWidth}}>
+        {questionText()}
+      </div>
+
+
+    <div>
+     
+    </div>
+  );
+
+    </div>
+  );
+}
+
+//return null;
+
+
+export default BarSite;
---
+++ CourseInfo.js
@@ -0,0 +1,316 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from "react-dom";
+import * as d3 from "d3";
+import AnimatedPieHooksdays from './doughnut_course_time.js';
+import AnimatedPieHooksworked from './doughnut_course_worktime.js';
+
+
+function loadData(){
+    try {
+      const storage = JSON.parse(window.localStorage.getItem("data"));
+      return storage || [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+function write_name(){
+  let myData = loadData();
+  var name = myData[0].id; 
+  return <h1>{name}</h1>;
+}
+
+function hours_to_min(value){
+  var hours = value/60
+  return hours
+}
+
+function days_tot(){
+  let myData = loadData();
+  var endDate = new Date(myData[0].endDate)
+  var todaysDate = new Date()
+
+  var difference_In_Time = endDate.getTime() - todaysDate.getTime();
+
+
+  var difference_In_Days = difference_In_Time / (1000 * 3600 * 24); 
+  
+  if( difference_In_Days % 1 < 0.5){
+    difference_In_Days = Math.floor(difference_In_Days)
+  }
+  else
+  {
+    difference_In_Days= Math.ceil(difference_In_Days)
+  }
+
+  
+  return difference_In_Days
+
+}
+
+function day_span(){
+    let myData = loadData();
+
+ var endDate = new Date(myData[0].endDate)
+  var startDate = new Date(myData[0].startDate)
+
+
+  var difference_In_Time = endDate.getTime() - startDate.getTime();
+  if(difference_In_Time < 0){
+    return 0
+  }
+  else{
+
+    var difference_In_Days = difference_In_Time / (1000 * 3600 * 24); 
+   
+    if( difference_In_Days % 1 < 0.5){
+        difference_In_Days = Math.floor(difference_In_Days)
+     }
+    else
+      {
+       difference_In_Days= Math.ceil(difference_In_Days)
+      }
+      
+    return difference_In_Days
+  }
+  
+}
+
+function hour_donut_text() { //Måste ändra vad myData[0]. är för nått
+    let myData = loadData();
+
+  var hour_worked = (myData[0].totTime)/ 60;
+  
+  if( hour_worked % 1 < 0.5){
+        hour_worked = Math.floor(hour_worked)
+     }
+  else
+     {
+       hour_worked= Math.ceil(hour_worked)
+     }
+
+  var tot_time = (myData[0].totTime)/ 60;
+
+  if( tot_time % 1 < 0.5){
+        tot_time = Math.floor(tot_time)
+     }
+  else
+     {
+       tot_time= Math.ceil(tot_time)
+     }
+    console.log('hour_worked')
+     console.log(hour_worked)
+     console.log('tot_time')
+     console.log(tot_time)
+
+     return <p>{hour_worked} / {tot_time}</p>
+
+}
+
+function day_donut_text() {
+    let myData = loadData();
+
+  return <p>{days_tot(myData)}</p>
+}
+
+
+function create_fake_array_two(myData){
+var dayDiff_nbr = days_tot(myData)
+
+var dayDiff_per = day_span(myData)
+
+if(((myData[0].value)-(myData[0].totAddedTime))<0){
+  var recomend_left = 0
+
+}
+else
+{
+  var recomend_left=(myData[0].value)-(myData[0].totAddedTime)
+}
+
+  var fakeArray = [
+    {
+        "id": myData[0].id,
+        "label": myData[0].label,
+        "value":  myData[0].value, //rec min
+        "addedTime":  myData[0].addedTime, //gjort min idag
+        "totAddedTime":  myData[0].totAddedTime, //gjort totalt
+        "color":  '#AE81FF',
+        "startDate": myData[0].startDate, //MM/DD/ÅÅÅÅ  30dar skillnad
+        "endDate": myData[0].endDate,
+        "recom": myData[0].totAddedTime
+    },
+    {
+       "id": "dummy",
+        "label": "dummy_",
+        "value": 100, //rec min
+        "addedTime": 0, //gjort min idag
+        "totAddedTime":0, //gjort totalt
+        "color": "#242b2b",
+        "startDate":"12/01/2019", //MM/DD/ÅÅÅÅ  30dar skillnad
+        "endDate":"12/27/2019",
+        "recom": recomend_left
+    }
+  ];
+
+  return fakeArray
+}
+
+function create_fake_array(myData){
+var dayDiff_nbr = days_tot(myData)
+
+var dayDiff_per = day_span(myData)
+
+
+  var fakeArray = [
+    {
+        "id": myData[0].id,
+        "label": myData[0].label,
+        "value":  myData[0].value, //rec min
+        "addedTime":  myData[0].addedTime, //gjort min idag
+        "totAddedTime":  myData[0].totAddedTime, //gjort totalt
+        "color":  myData[0].color,
+        "startDate": myData[0].startDate, //MM/DD/ÅÅÅÅ  30dar skillnad
+        "endDate": myData[0].endDate,
+        "dayDiff": dayDiff_nbr
+    },
+    {
+       "id": "dummy",
+        "label": "dummy_",
+        "value": 100, //rec min
+        "addedTime": 0, //gjort min idag
+        "totAddedTime":0, //gjort totalt
+        "color": "#242b2b",
+        "startDate":"12/01/2019", //MM/DD/ÅÅÅÅ  30dar skillnad
+        "endDate":"12/27/2019",
+        "dayDiff": dayDiff_per
+    }
+  ];
+
+  return fakeArray
+}
+
+function goal_text(myData) {
+    var tot_value_min = myData[0].value
+    var tot_worked_min = myData[0].totAddedTime
+
+    var days_left= days_tot(myData)
+    console.log(days_left)
+    var work_a_day = (tot_value_min-tot_worked_min)/days_left;
+
+    var hours= Math.floor(work_a_day/60);
+    console.log(hours+"Hours")
+    var minutes = (work_a_day % 60);
+    console.log(minutes+"min")
+  
+   if(minutes === 0){
+     return hours+'h';
+   }
+   if(hours === 0){
+     return Math.floor(minutes)+"min"
+    }
+
+   return hours+'h '+ Math.floor(minutes)+'min';
+
+
+
+    return <p>Goal: {hours}h {Math.floor(minutes)}</p>
+}
+
+
+function courseinfo() {
+  return (
+    <div className="center">
+        {/*<div>
+          <button onClick={changeData}>Transform</button>
+        </div>*/}
+        <div className="appHeader head fitText">
+           {write_name(myData)}
+         
+        </div>
+
+          {/*deadlines*/}
+        <div className="deadlines">
+          <p id="dead_title">Deadlines</p>
+
+          <div className="deadlines_in">
+            <p>Tenta 03/05-20</p>
+          </div>
+
+        </div>
+
+        <div id="wrapper">
+          
+          <div id="first">
+            <AnimatedPieHooksworked
+              data={create_fake_array_two(myData)}
+              width={170}
+              height={170}
+              innerRadius={60}
+              outerRadius={84}
+            />
+              
+            <div id="lhsText">
+              {hour_donut_text(myData)}
+              <p>hours</p>
+            </div>
+         
+          </div>
+
+          <div id="second">
+           <AnimatedPieHooksdays
+            data={create_fake_array(myData)}
+            width={170}
+            height={170}
+            innerRadius={60}
+            outerRadius={84}
+            />
+
+            <div id="rhsText">
+              {day_donut_text(myData)}
+              <p>days left</p>
+            </div>
+            
+          </div>
+
+        </div>
+
+         <div className="goal">
+           <p>Goal: {goal_text(myData)} per day</p>
+
+         </div>
+
+        <div className="divButtonCourseInfo">
+            <button onClick={() =>( myData[0].id, myData[0].startDate,myData[0].endDate,myData[0].value)} className="reportButton_CourseInfo center">
+               <p className="buttonText" id="reportButtonText">Report time</p>
+            </button>
+          
+            <button onClick={() =>( myData[0].id, myData[0].startDate,myData[0].endDate,myData[0].value)} className="editButton center">
+               <p className="buttonText" id="editButtonText">Edit activity</p>
+           </button>
+        </div>
+   {/*Footer*/}
+      <div className="App-bottom">
+      <p className="dots">Bottom Header</p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default courseinfo;
+
+var myData = [
+    {
+        "id": "TNA004",
+        "label": "Analys2",
+        "value": 876, //rec min
+        "addedTime": 110, //gjort min idag
+        "totAddedTime":110, //gjort totalt
+        "color": "#f92672",
+        "startDate":"12/02/2019", //MM/DD/ÅÅÅÅ
+        "endDate":"12/05/2019"
+    }
+];
